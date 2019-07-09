0071368d863eb5b12f1bfd8558c6e50dcdaad654
Luca Stasio <lucastasio@MBP-di-Luca.itresources.it>
2019-07-09 11:59:26 +0200

    some method renames

--- gm/gear.go
+++ gm/gear.go
@@ -15,8 +15,14 @@ type Gear interface {
 
 // BaseGear is the Machinery most basic building block structure.
 // If a component want to be loaded into the app should derive from this.
+type BaseGear struct {
+	uname string
+}
+
+// ConfigurableGear is a BasicGear with a config map structure.
 // However, a gear will not be configured if it does not implement
 // the Configurable interface.
-type BaseGear struct {
+type ConfigurableGear struct {
+	BaseGear
 	Config map[string]interface{}
 }
